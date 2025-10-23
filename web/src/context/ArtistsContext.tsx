447319a6c0fac5a1ae40744dcdbaf8df9571775c
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2025-10-23 16:02:19 +0200

    Ready for api requests

--- web/src/context/ArtistsContext.tsx
+++ web/src/context/ArtistsContext.tsx
@@ -5,6 +5,9 @@ import { mockArtists } from "@features/artists/mockData/mockArtists";
 // Fake API for demonstration
 const fetchArtists = async () => {
     // Replace with real API call: await fetch("/api/artists").then(res => res.json())
+    /*const res = await fetch("/api/auctions");
+    if (!res.ok) throw new Error("Failed to fetch auctions");
+    return await res.json() as Promise<Auction[]>;*/
     return new Promise<Artist[]>((resolve) => setTimeout(() => resolve(mockArtists), 500));
 };
 
--- web/src/context/AuctionsContext.tsx
+++ web/src/context/AuctionsContext.tsx
@@ -5,6 +5,9 @@ import { mockAuctions } from "@features/paintings/mockData/mockAuctions";
 // Fake API for demonstration
 const fetchAuctions = async () => {
     // Replace with real API call: await fetch("/api/auctions").then(res => res.json())
+    /*const res = await fetch("/api/auctions");
+    if (!res.ok) throw new Error("Failed to fetch auctions");
+    return await res.json() as Promise<Auction[]>;*/
     return new Promise<Auction[]>((resolve) => setTimeout(() => resolve(mockAuctions), 500));
 };
 
